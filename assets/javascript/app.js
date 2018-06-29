bf4830f712e7ee362d37621f7ec53b416e6883e1
Astronaut Group <19156046+alexanderjbeal@users.noreply.github.com>
2018-06-28 21:17:57 -0500

    Development 2018-06-28

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+var recipe = $(this).attr("data-name");
+var queryURL = 'https://api.edamam.com/search?q=chicken&app_id=66ec94ca&app_key=2ac8781d2e3a0ea944c221aaaa9ccf5f';
+var resultsArray = []
+
+function displayRecipe() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    
+    .then(function(response) {
+        //
+        resultsArray = response.hits;
+
+        for (var i = 0; i < 10; i++) {
+            // Recipe Image
+            var recipeDiv = $(`<div id='${i}' class='recipe-item'>`);
+            var imgURL = response.hits[i].recipe.image;
+            var recipeImage = $('<img>').attr({
+                src: imgURL,
+                height: '200px',
+                id: "recipe"+i,
+            })
+            recipeDiv.append(recipeImage);
+
+            // Recipe Name
+            var name = response.hits[i].recipe.label;
+            var recipeName = $('<p>').html(name);
+            recipeDiv.append(recipeName);
+
+            
+         
+            $('.recipe-rating').html(`Calories: ${response.hits[i].recipe.calories}`);
+
+            // Log the data in the console as well
+        
+            console.log(response.hits[i].recipe.image);
+
+            $("#recipe").append(recipeDiv);
+
+        }
+    })
+}
+
+$(document).on("click", displayRecipe);
